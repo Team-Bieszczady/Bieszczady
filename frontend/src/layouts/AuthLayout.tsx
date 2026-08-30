9bbb2ea6cb73fb9762395e4bcb76efe2296b56ed
viko4kka <kasprikviktoria4@gmail.com>
2026-08-30 20:17:53 +0200

    fix: authlayout fix

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,6 +1,20 @@
+import { Outlet } from 'react-router';
+import logo from '../assets/logo_bieszczadzki_ul.jpg';
 
 export default function AuthLayout() {
   return (
-    <div>AuthLayout</div>
-  )
+    <div className="min-h-screen bg-[#eeeeee] flex items-center justify-center px-4 py-8">
+      <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-6 max-sm:p-4">
+        <div className="flex justify-center mb-2">
+          <img
+            src={logo}
+            alt="Bieszczadzki UL"
+            className="h-16 700:h-20 w-16 700:w-20"
+          />
+        </div>
+
+        <Outlet />
+      </div>
+    </div>
+  );
 }
